3ff14ab374f462c3881c8f8d8cc7d98d02087fa9
TND <txtx8759@gmail.com>
2025-09-05 16:21:39 +0500

    beta Release
    This is non final. Site (as content) may changing and other.
    
    -TheresNoDeath

--- ONAGASRPage/news/script.js
+++
@@ -1,8 +0,0 @@
-async function fetchHtmlAsText(url) {
-    return await (await fetch(url)).text();
-
-}
-
-async function GetNews_info(id) {
-    document.getElementById("News").innerHTML = await fetchHtmlAsText("News/news_${id}.html");
-}
